3795f34f4d2f848c8e85743e13a0da9683134bd3
Dror Harari <DrorHarari@users.noreply.github.com>
2018-09-29 04:08:52 +0300

    Fix build

--- etc/build.py
+++ etc/build.py
@@ -4,18 +4,22 @@ import os
 import zipfile
 
 PACKAGE_NAME="Ppl"
+FILES=["ppl.py","ppl.ini","ppl.ico","LICENSE","etc/make_contacts.py"]
 
 ETC_FOLDER=Path(__file__).parent
 PACKAGE_FOLDER=ETC_FOLDER.parent
 PACKAGE_FILE=Path.joinpath(PACKAGE_FOLDER, f"{PACKAGE_NAME}.keypirinha-package")
 
-zf = zipfile.ZipFile(PACKAGE_FILE,'w',zipfile.ZIP_DEFLATED)
+print(f"Creating package file {PACKAGE_FILE}")
 
-# File to include in the package
-os.chdir(PACKAGE_FOLDER)
-zf.write('ppl.py')
-zf.write('ppl.ini')
-zf.write('ppl.ico')
-zf.write('LICENSE')
+try:
+    zf = zipfile.ZipFile(PACKAGE_FILE,'w',zipfile.ZIP_DEFLATED)
+    for f in FILES:
+        zf.write(Path.joinpath(Path('..'), f), f)
 
-zf.close()
+    zf.close()
+except Exception as exc:
+    print(f"Failed to create package {PACKAGE_NAME}. {exc}")
+    os._exit(1)
+
+print(f"Done")
