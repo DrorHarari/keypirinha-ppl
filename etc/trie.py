578222dfc2b611336b6c54bfe7c3f57a225dc342
yonatanh20 <yonatan.harari@gmail.com>
2021-10-05 21:44:13 +0300

    Fixed print bug

--- etc/trie.py
+++ etc/trie.py
@@ -119,6 +119,6 @@ if __name__ == '__main__':
     words = 'hello goodbye help gerald gold tea ted team to too tom stan standard money'
     for word in words.split():
         trie.add(word)
-    print "'goodbye' in trie: ", trie.has_word('goodbye')
-    print trie.start_with_prefix('g')
-    print trie.start_with_prefix('to')
+    print("'goodbye' in trie: ", trie.has_word('goodbye'))
+    print(trie.start_with_prefix('g'))
+    print(trie.start_with_prefix('to'))
