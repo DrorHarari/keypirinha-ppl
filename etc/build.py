322da713d1ea5589ecf7fbd9416eb691abedd380
yonatanh20 <yonatan.harari@gmail.com>
2021-10-05 22:40:51 +0300

    Git Actions (#5)
    * Git Actions
    
    * Fixed print bug
    
    * Removed Windows import from build + refactoring
    
    * Removed Capital letter from package

--- etc/build.py
+++ etc/build.py
@@ -2,19 +2,18 @@
 from pathlib import Path
 import os
 import zipfile
-import msvcrt
 
-PACKAGE_NAME="Ppl"
-FILES=["ppl.py","ppl.ini","ppl.ico","LICENSE", "etc/make_contacts.py","etc/sample-contacts.vcf"]
+PACKAGE_NAME = "ppl"
+FILES = ["ppl.py", "ppl.ini", "ppl.ico", "LICENSE", "etc/make_contacts.py", "etc/sample-contacts.vcf"]
 
-ETC_FOLDER=Path(__file__).parent
-PACKAGE_FOLDER=ETC_FOLDER.parent
-PACKAGE_FILE=Path.joinpath(PACKAGE_FOLDER, f"{PACKAGE_NAME}.keypirinha-package")
+ETC_FOLDER = Path(__file__).parent
+PACKAGE_FOLDER = ETC_FOLDER.parent
+PACKAGE_FILE = Path.joinpath(PACKAGE_FOLDER, f"{PACKAGE_NAME}.keypirinha-package")
 
 print(f"Creating package file {PACKAGE_FILE}")
 
 try:
-    zf = zipfile.ZipFile(PACKAGE_FILE,'w',zipfile.ZIP_DEFLATED)
+    zf = zipfile.ZipFile(PACKAGE_FILE,'w', zipfile.ZIP_DEFLATED)
     for f in FILES:
         zf.write(Path.joinpath(Path('..'), f), f)
 
@@ -25,7 +24,6 @@ except Exception as exc:
         zf.close()
         os.remove(PACKAGE_FILE)
     print(f"Failed to create package {PACKAGE_NAME}. {exc}")
-    msvcrt.getch()
     os._exit(1)
 
-print(f"Done")
+print("Done")
