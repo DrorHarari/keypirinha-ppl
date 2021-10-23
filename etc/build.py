e042e7e738d602c81d2dfa0c55bbec9764b44f91
Dror Harari <dror.harari@gmail.com>
2021-10-23 04:00:35 +0300

    Added version to ini file

--- etc/build.py
+++ etc/build.py
@@ -2,20 +2,29 @@
 from pathlib import Path
 import os
 import zipfile
-import msvcrt
 
-PACKAGE_NAME="Ppl"
-FILES=["ppl.py","ppl.ini","ppl.ico","LICENSE", "etc/make_contacts.py","etc/sample-contacts.vcf"]
+PACKAGE_NAME = "ppl"
+PACKAGE_VERSION = f" (version {os.environ['PACKAGE_VERSION']})" \
+    if "PACKAGE_VERSION" in os.environ else ""
+FILES = ["ppl.py", "ppl.ini", "ppl.ico", "LICENSE", "etc/make_contacts.py", "etc/sample-contacts.vcf"]
 
-ETC_FOLDER=Path(__file__).parent
-PACKAGE_FOLDER=ETC_FOLDER.parent
-PACKAGE_FILE=Path.joinpath(PACKAGE_FOLDER, f"{PACKAGE_NAME}.keypirinha-package")
+# Following is common code
+ETC_FOLDER = Path(__file__).parent
+PACKAGE_FOLDER = ETC_FOLDER.parent
+PACKAGE_FILE = Path.joinpath(PACKAGE_FOLDER, f"{PACKAGE_NAME}.keypirinha-package")
 
 print(f"Creating package file {PACKAGE_FILE}")
 
 try:
-    zf = zipfile.ZipFile(PACKAGE_FILE,'w',zipfile.ZIP_DEFLATED)
+    zf = zipfile.ZipFile(PACKAGE_FILE,'w', zipfile.ZIP_DEFLATED)
     for f in FILES:
+        if PACKAGE_VERSION and f == f"{PACKAGE_NAME}.ini":  # Replace {version} in package configuration template
+            with open(Path.joinpath(Path('..'), f), "r", encoding="utf-8") as inif:
+                ini = ''.join(inif.readlines())
+                ini = cvtini.replace("{#version#}", PACKAGE_VERSION)
+                zf.writestr(f, ini.encode())
+            continue
+
         zf.write(Path.joinpath(Path('..'), f), f)
 
     zf.close()
@@ -25,7 +34,6 @@ except Exception as exc:
         zf.close()
         os.remove(PACKAGE_FILE)
     print(f"Failed to create package {PACKAGE_NAME}. {exc}")
-    msvcrt.getch()
     os._exit(1)
 
-print(f"Done")
+print("Done")
