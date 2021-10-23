ef6a480faee65b5b8989f7f11652c42b1ba6e262
Dror Harari <DrorHarari@users.noreply.github.com>
2021-10-23 03:47:16 +0300

    Update build.py

--- etc/build.py
+++ etc/build.py
@@ -4,8 +4,11 @@ import os
 import zipfile
 
 PACKAGE_NAME = "ppl"
+PACKAGE_VERSION = f" (version {os.environ['PACKAGE_VERSION']})" \
+    if "PACKAGE_VERSION" in os.environ else ""
 FILES = ["ppl.py", "ppl.ini", "ppl.ico", "LICENSE", "etc/make_contacts.py", "etc/sample-contacts.vcf"]
 
+# Following is common code
 ETC_FOLDER = Path(__file__).parent
 PACKAGE_FOLDER = ETC_FOLDER.parent
 PACKAGE_FILE = Path.joinpath(PACKAGE_FOLDER, f"{PACKAGE_NAME}.keypirinha-package")
@@ -15,6 +18,13 @@ print(f"Creating package file {PACKAGE_FILE}")
 try:
     zf = zipfile.ZipFile(PACKAGE_FILE,'w', zipfile.ZIP_DEFLATED)
     for f in FILES:
+        if PACKAGE_VERSION and f == f"{PACKAGE_NAME}.ini":  # Replace {version} in package configuration template
+            with open(Path.joinpath(Path('..'), f), "r", encoding="utf-8") as inif:
+                ini = ''.join(inif.readlines())
+                ini = cvtini.replace("{#version#}", PACKAGE_VERSION)
+                zf.writestr(f, ini.encode())
+            continue
+
         zf.write(Path.joinpath(Path('..'), f), f)
 
     zf.close()
